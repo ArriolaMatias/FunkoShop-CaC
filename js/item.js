81f3f57219337c2746f8d411a0175410461c2fd9
ArriolaMatias <arriolamattias@gmail.com>
2023-10-29 19:09:55 -0300

    agregado archivo js para agregar funcionalidad a los botones + y - en item.html

---
+++ js/item.js
@@ -0,0 +1,12 @@
+const add = document.querySelector("#add");
+const substract = document.querySelector("#substract");
+
+const quantity = document.querySelector("#quantity");
+
+console.log(quantity);
+
+add.addEventListener('click' , () => quantity.value = Number(quantity.value) +1);
+substract.addEventListener('click' , () => {
+    if (Number(quantity.value) > 0)
+   quantity.value = Number(quantity.value) -1;
+});
