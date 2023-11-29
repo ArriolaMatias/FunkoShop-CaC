07032dbc5dddae1c62b01fec58213722b00a2dc2
ArriolaMatias <arriolamattias@gmail.com>
2023-11-28 23:50:10 -0300

    Implementadas algunas vistas de template engines

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,5 +1,9 @@
 const mainControllers = {
-    home: (req, res) => { res.send("Ruta para la vista de Home")},
+    home: (req, res) => {     
+        res.render('index.ejs', 
+        { 
+            title: 'Home | Funkoshop'
+    })},
     contact: (req, res) => { res.send("Ruta para la vista de Contacto")},
     about: (req, res) => { res.send("Ruta para la vista de About")},
     faqs: (req, res) => { res.send("Ruta para la vista de Preguntas Frecuentes")}
--- src/controllers/shopController.js
+++ src/controllers/shopController.js
@@ -1,6 +1,12 @@
 const shopControllers = {
-    shop: (req, res) => { res.send("Ruta para la vista de Shop")}, 
-    item: (req, res) => {res.send("Ruta para encontrar un producto a partir de un id")},
+    shop: (req, res) => { res.render('./shop/shop.ejs', {
+        title: 'Shop | Funkoshop'
+    })}, 
+
+    item: (req, res) => {res.render('./shop/item.ejs', {
+        title: 'Item cargado dinámicamente | Funkoshop'
+    })},
+    
     add_item: (req, res) => { res.send("Ruta para agregar el producto actual al carrito")},
     cart: (req, res) => { res.send("Ruta para ver el carrito")},
     cart_to_checkout: (req, res) => { res.send("Ruta para ir a la pagina de pagos")},
