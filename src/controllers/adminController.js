89016571ba272c494d8b72022cf258e0b9124d52
Lucas Colque <lucascolque35@gmail.com>
2023-12-08 13:42:55 -0300

    Agregando multer al proyecto

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,10 +1,11 @@
 const Utilities = require("../utilities/json-utilities.js");
+const { unlink } = require("fs");
 const {
     getAllFunkosFromDB,
     getFunkoFromDB,
     addFunkoFromDB,
     deleteFunkoFromDB,
-    updateFunkoFromDB
+    updateFunkoFromDB,
 } = require("../models/model.js");
 const { getAllLicencesFromDB } = require("../models/licence.js");
 const { getAllCategoriesFromDB } = require("../models/category.js");
@@ -29,13 +30,17 @@ const adminControllers = {
                 licences: responseLicence,
                 categories: responseCategory,
             });
-        } catch (error) { 
+        } catch (error) {
             //!CODIGO DE ERROR
         }
     },
     create_post: async (req, res) => {
+        const funkoData = req.body;
+        funkoData.image_front = `/storage/${req.files["image_front"][0].filename}`;
+        funkoData.image_back = `/storage/${req.files["image_back"][0].filename}`;
         try {
-            const funkoData = req.body;
+            // funkoData.image_front = `/${req.file.filename}`
+            // funkoData.imagen_back =
             await addFunkoFromDB(funkoData);
             res.redirect("/admin");
         } catch (error) { }
@@ -58,8 +63,35 @@ const adminControllers = {
     },
     edit_post: async (req, res) => {
         try {
+
             const id = req.params.id;
+            const imgAntFunko = await getFunkoFromDB(id);
             const funkoData = req.body;
+            if (req.files && Object.keys(req.files).length > 0) {
+
+                if (req.files.image_front !== undefined) {
+                    funkoData.image_front = `/storage/${req.files.image_front[0].filename}`;
+                    unlink(`public/img/${imgAntFunko.image_front}`, (err) => {
+                        if (err) {
+                            console.log("Ocurrio un error al eliminar la imagen front");
+                            return;
+                        }
+                    });
+                }
+                if (req.files.image_back !== undefined) {
+                    funkoData.image_back = `/storage/${req.files.image_back[0].filename}`;
+                    unlink(`public/img/${imgAntFunko.image_back}`, (err) => {
+                        if (err) {
+                            console.log("Ocurrio un error al eliminar la imagen back");
+                            return;
+                        }
+                    });
+                }
+
+            } else {
+                funkoData.image_front = imgAntFunko.image_front;
+                funkoData.image_back = imgAntFunko.image_back;
+            }
             await updateFunkoFromDB(funkoData, id);
             res.redirect("/admin");
         } catch (error) {
@@ -67,8 +99,8 @@ const adminControllers = {
         }
     },
     delete: async (req, res) => {
+        const id = req.params.id;
         try {
-            const id = req.params.id;
             const response = await getFunkoFromDB(id);
             res.render("admin/delete", {
                 title: `Eliminar producto: ${response.product_name} | FunkoShop`,
@@ -81,9 +113,23 @@ const adminControllers = {
     delete_confirm: async (req, res) => {
         try {
             const id = req.params.id;
-            await deleteFunkoFromDB(id);
+            const funko = await deleteFunkoFromDB(id);
+            if (funko) {
+                unlink(`public/img/${funko.image_front}`, (err) => {
+                    if (err) {
+                        console.log("Ocurrio un error al eliminar la imagen front");
+                        return;
+                    }
+                });
+                unlink(`public/img/${funko.image_back}`, (err) => {
+                    if (err) {
+                        console.log("Ocurrio un error al eliminar la imagen back");
+                        return;
+                    }
+                });
+            }
             res.redirect("/admin");
-        } catch (error) { 
+        } catch (error) {
             //!ERROR CODIGO
         }
     },
