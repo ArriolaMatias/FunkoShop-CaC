7f9fece4da9f87afa4bb81c0a83ce1819b821826
ArriolaMatias <arriolamattias@gmail.com>
2023-12-27 17:00:01 -0300

    Se declara utilizacion de las validaciones en la pantalla de Login

--- src/routes/auth.Routes.js
+++ src/routes/auth.Routes.js
@@ -7,7 +7,7 @@ const authControllers = require('../controllers/authController.js');
 router.use(bodyParser.json());
 
 router.get('/login', authControllers.login);
-router.post('/login', authControllers.login_post);
+router.post('/login', validator.login, authControllers.login_post);
 router.post('/register', validator.newRegister, authControllers.register_post);
 router.get('/register', authControllers.register);
 router.get('/logout', authControllers.logout);
