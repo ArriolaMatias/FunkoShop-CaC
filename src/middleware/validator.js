c54e852ba1774dc10b9ca611fee5d58251357e0a
ArriolaMatias <arriolamattias@gmail.com>
2023-12-27 17:04:36 -0300

    Implementada validacion para el form de Login

--- src/middleware/validator.js
+++ src/middleware/validator.js
@@ -1,8 +1,8 @@
 const {body} = require('express-validator');
 const {addUserFromDB, getUserByEmailFromDB} = require('../models/users');
 
-newRegister = 
-[body('name').exists().notEmpty().escape().withMessage("El campo es obligatorio"),
+newRegister = [
+  body('name').exists().notEmpty().escape().withMessage("El campo es obligatorio"),
 body('lastname').notEmpty().withMessage("El campo es obligatorio").isAlpha().withMessage("No se aceptan números."),
 body('email').notEmpty().withMessage('El campo es obligatorio').trim().isEmail()
             .custom( async (email, {req}) => {
@@ -16,9 +16,26 @@ body('rePassword').trim().custom( async (rePassword, {req}) => {
     if(password != rePassword){
       throw new Error('Las contraseñas deben ser iguales')
     }
-  }),];
+  })
+];
+
+login = [
+  body('email').notEmpty().withMessage('El campo es obligatorio').isEmail().withMessage("No valido")
+            .custom( async (email, {req}) => {
+                if (await getUserByEmailFromDB(email) == undefined){
+                    throw new Error('No hay ningun usuario con el correo ingresado.');
+                }
+}),
+body('password').notEmpty().withMessage("Debes introducir la contraseña").custom( async (email, {req}) => {
+  const user = await getUserByEmailFromDB(req.body.email)
+  if( user != undefined  && req.body.password != user.password){
+    throw new Error('La contraseña es incorrecta')
+  }
+})
+]
 
 
 module.exports = {
-    newRegister
+    newRegister,
+    login
 }
